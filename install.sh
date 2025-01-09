7645f11b73ab835d5813987738cb3da565ca4374
diabolo511 <93436829+diabolo511@users.noreply.github.com>
2025-01-09 22:01:42 +0100

    Update install.sh
    Update to needed Repos

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo pacman -Syuu --noconfirm
 
 ### --- Install packages --- ###
 
-sudo pacman -S --needed --noconfirm base-devel polkit-gnome libsecret gnome-keyring
+sudo pacman -S --needed --noconfirm base-devel
 
 ### --- Install AUR Helper --- ###
 
@@ -15,51 +15,7 @@ cd ../ && rm -rf yay
 
 ### --- Install Software --- ###
 
-if [[ "$1" == "--X11" ]]; then
-  pkgs=(
-  "polybar" 
-  "dunst" 
-  "feh" 
-  "maim" 
-  "xdg-desktop-portal" 
-  "playerctl" 
-  "picom" 
-  "xorg-setxkbmap" 
-  "nwg-look" 
-  "rofi" 
-  "xdg-desktop-portal-gtk"
-  )
-  for pkg in "${pkgs[@]}"; do
-    echo "-- Installing: $pkg"
-    sudo pacman -S --needed --noconfirm ${pkg}
-  done
 
-  mkdir ~/.config/rofi/
-  ln -sf $PWD/config/rofi/config.rasi ~/.config/rofi/
-  ln -sf $PWD/config/rofi/themes/ ~/.config/rofi/
-
-  mkdir ~/.config/polybar/
-  ln -sf $PWD/config/polybar/colors.ini ~/.config/polybar/
-  ln -sf $PWD/config/polybar/config.ini ~/.config/polybar/
-  ln -sf $PWD/config/polybar/modules.ini ~/.config/polybar/
-  ln -sf $PWD/config/polybar/scripts/ ~/.config/polybar/
-
-  mkdir ~/.config/dunst/
-  ln -sf $PWD/config/dunst/dunstrc ~/.config/dunst/
-
-  mkdir ~/.config/picom/
-  ln -sf $PWD/config/picom/picom.conf ~/.config/picom/
-
-  mkdir ~/.config/bspwm
-  ln -sf $PWD/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-  chmod 755 $PWD/config/bspwm/bspwmrc
-
-  mkdir -p ~/.config/sxhkd
-  ln -s $PWD/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-  chmod 644 $PWD/config/sxhkd/sxhkdrc
-
-  ln -sf $PWD/config/.Xresources ~/
-fi
 
 pkgs=(
   "git"
@@ -67,7 +23,7 @@ pkgs=(
   "bluez-utils"
   "cups"
   "cups-pdf"
-  "fish"
+  "zsh"
   "which"
   "github-cli"
   "fzf"
@@ -95,13 +51,10 @@ pkgs=(
   "reflector"
   "bat"
   "fd"
-  "fisher"
   "starship"
   "xclip"
   "tree"
   "ripgrep"
-  "xorg-xsetroot"
-  "wmctrl"
 # ----------- #  
   "bitwarden"
   "vlc"
@@ -110,14 +63,12 @@ pkgs=(
   "linux-firmware-qlogic"
   "spotify-launcher"
   "ghostty"
-  "lazygit"
 # ---------- # 
   "cmake"
   "clang"
   "ninja"
   "gdb"
-  "python-psutil"
-  "python-distutils-extra"
+  "python"
   "go"
   "nodejs"
   "npm"
@@ -150,9 +101,7 @@ aurpkgs=(
   "anydesk-bin"
   "discord-canary"
   "discord-canary-wayland-hook"
-  "intellij-idea-community-edition-jre"
   "github-desktop-bin"
-  "swift-bin"
   "brave-bin"
 )
 
@@ -178,16 +127,9 @@ sudo ufw default reject
 sudo ufw enable
 sudo systemctl enable ufw.service
 
-sudo systemctl enable nvidia-suspend.service
-sudo systemctl enable nvidia-hibernate.service
-sudo systemctl enable nvidia-resume.service
+### --- ZSH --- ###
 
-### --- Fish --- ###
-
-chsh -s $(which fish)
-mkdir ~/.config/fish
-ln -sf $PWD/config/fish/config.fish ~/.config/fish/
-ln -sf $PWD/config/fish/fish_plugins ~/.config/fish/
+chsh -s $(which zsh)
 
 ### --- Starship --- ###
 
